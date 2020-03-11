3ddd6765d248fe844a2596760c24734b93cb60dc
somyakajla <somyakajla90@gmail.com>
2020-03-11 00:56:00 -0700

    update port

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,7 @@
 const env = process.env.NODE_ENV || 'development';
 
 if (env == 'development') {
-    process.env["PORT"] = process.env.PORT || 8080;
+    process.env["PORT"] = process.env.PORT || 3000;
     process.env["MONGODB_URI"] = process.env.CUSTOMCONNSTR_mongoDbConnStr || 'mongodb://saas-construction-db:t6E8S7Tz7qIND930OlEbbFeNDf9MSEVrEh5hKn08orNUZ2dIibRRKLSQsPKVvOeUvsLqa2trf70y1aJi0NUIuw==@saas-construction-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@saas-construction-db@';
     process.env["JWT_SECRET"] = 'SECRET#123';
     process.env["JWT_EXP"] = '60m';
