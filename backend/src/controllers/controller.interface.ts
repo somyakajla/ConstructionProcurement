44d7a209d78d1e836c5a55cd2f3428d8a0b581b4
somyakajla <somyakajla90@gmail.com>
2020-03-11 10:06:19 -0700

    backend changes

--- backend/src/controllers/controller.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-
-export interface IController {
-    route: string;
-    router: Router;
-}
--- backend/src/environment.ts
+++
@@ -1,6 +0,0 @@
-export const baseUrl = process.env.BaseUrl || 'http://localhost';
-export const port = process.env.PORT || 8080;
-export const mongoDbUrl = process.env.CUSTOMCONNSTR_mongoDbConnStr || 'mongodb://saas-construction-db:DaFbVtp6RLHPk3u6U0wx1VrofW5BPpxIDKGwD7pZI3Xr0vbtsIK6hDcavAM5lTWl3tLrUxw6jOM9q1cj7mHV9g==@saas-construction-db.documents.azure.com:10255/?ssl=true&replicaSet=globaldb';
-export const allowedCors = process.env.AllowedCors;
-export const sendgridApiKey = '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';
-export const loginSecretKey = 'IlWyysvNxShnAFwZT5eyJh5+mVpwEhe1bZZE1iSJrCRfANlztFk1s7SYj+Z2bDSONmbIHSjSzYRurVZsP+e5hjBmSBAkudqABuQzNQ3AtlOPhUoDo0jhVe2HikN4p5QIAjYgmfSBxO5glz0fMMr+uU92Kb2g2ZDBNgeX+o2PXYrAT5+ErN7rhWvTwkRKtBL9Utr90AE6NMjoNEoXTdvxJsYSx813N+AFEOCJAkRGZp6S6oT/DfggDdWL6Vyt31mI+x6sUPgsI1SpKOD5yjsCaXoZfH1lx+Lb/f5v+iXtnub7cYhF6y1JqCoH03vdR8npBuYvril8PwvE+Kh1ugrR3w==';
