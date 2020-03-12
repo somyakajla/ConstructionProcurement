edcd875be2c489248b1d6b06824c31b4fc3ed0ca
somyakajla <somyakajla90@gmail.com>
2020-03-12 13:07:47 -0700

    mongodb key changes

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,12 +3,12 @@ const env = process.env.NODE_ENV || 'development';
 
 if (env == 'development') {
     process.env["PORT"] = process.env.PORT || 8080;
-    process.env["MONGODB_URI"] = process.env.CUSTOMCONNSTR_mongoDbConnStr || 'mongodb://saas-construction-db:t6E8S7Tz7qIND930OlEbbFeNDf9MSEVrEh5hKn08orNUZ2dIibRRKLSQsPKVvOeUvsLqa2trf70y1aJi0NUIuw==@saas-construction-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@saas-construction-db@';
+    process.env["MONGODB_URI"] = process.env.CUSTOMCONNSTR_mongoDbConnStr || 'mongodb://sasproject:3MRwQc3azxDJfOZ1cAOg7lqE9ffx0fmb2YQcJTuRcxiG3PdYS3s2qgzWMNrb8ajC7UVA1ibIKUlo9WueBbiNMg==@sasproject.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@sasproject@';
     process.env["JWT_SECRET"] = 'SECRET#123';
     process.env["JWT_EXP"] = '60m';
 } else {
     process.env["PORT"] = process.env.PORT || 8080;
-    process.env["MONGODB_URI"] = process.env.CUSTOMCONNSTR_mongoDbConnStr || 'mongodb://saas-construction-db:t6E8S7Tz7qIND930OlEbbFeNDf9MSEVrEh5hKn08orNUZ2dIibRRKLSQsPKVvOeUvsLqa2trf70y1aJi0NUIuw==@saas-construction-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@saas-construction-db@';
+    process.env["MONGODB_URI"] = process.env.CUSTOMCONNSTR_mongoDbConnStr || 'mongodb://sasproject:3MRwQc3azxDJfOZ1cAOg7lqE9ffx0fmb2YQcJTuRcxiG3PdYS3s2qgzWMNrb8ajC7UVA1ibIKUlo9WueBbiNMg==@sasproject.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@sasproject@';
     process.env["JWT_SECRET"] = 'SECRET#123';
     process.env["JWT_EXP"] = '2m';
 }
